c0156285cc410b244adfe4c39de339a024837879
FlyMachinee <1427632504@qq.com>
2025-05-10 18:01:55 +0800

    Init repository

---
+++ src/main/java/cn/edu/hit/artman/ArtmanApplication.java
@@ -0,0 +1,11 @@
+package cn.edu.hit.artman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtmanApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArtmanApplication.class, args);
+	}
+}
